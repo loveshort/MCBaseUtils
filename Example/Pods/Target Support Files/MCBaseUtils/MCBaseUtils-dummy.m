28bc87999cc51c94e9f989dbeb89c72445de3e55
mxx <271901088@qq.com>
2022-08-11 16:23:57 +0800

    提交

---
+++ Example/Pods/Target Support Files/MCBaseUtils/MCBaseUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MCBaseUtils : NSObject
+@end
+@implementation PodsDummy_MCBaseUtils
+@end
---
+++ Example/Pods/Target Support Files/Pods-MCBaseUtils_Example/Pods-MCBaseUtils_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MCBaseUtils_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MCBaseUtils_Example
+@end
